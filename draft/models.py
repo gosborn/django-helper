7ae1a421aff03d50d42224707c87b0c13232a477
Greg Osborn <greg.osborn@hireanesquire.com>
2017-08-22 21:28:18 -0400

    Adds services, adds stats field to player model

--- draft/models.py
+++ draft/models.py
@@ -35,6 +35,37 @@ class Player(models.Model):
     position = models.CharField(max_length=8, choices=Positions.OPTIONS)
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
+    nfl_team = models.CharField(max_length=8)  # this should be a model
+    bye_week = models.IntegerField()
+    standard_dev = models.FloatField()
+    nerd_rank = models.FloatField()
+    nerd_position_rank = models.FloatField()
+    nerd_overall_rank = models.FloatField()
+    nerd_id = models.IntegerField()
+
+    nerd_estimated_points = models.IntegerField()
+
+    #QB specific fields
+    completions = models.IntegerField()
+    passing_attempts = models.IntegerField()
+    passing_yards = models.IntegerField()
+    passing_tds = models.IntegerField()
+
+    # non-def fields
+    rush_attempts = models.IntegerField()
+    rush_yards = models.IntegerField()
+    rush_tds = models.IntegerField()
+    fumbles = models.IntegerField()
+    receptions = models.IntegerField()
+    receiving_yards = models.IntegerField()
+    receiving_tds = models.IntegerField()
+
+    # def fields
+    sacks = models.IntegerField()
+    interceptions = models.IntegerField()
+    fumble_recoveries = models.IntegerField()
+    def_tds = models.IntegerField()
+    special_team_tds = models.IntegerField()
 
 
 class DraftPick(models.Model):
---
+++ draft/services.py
@@ -0,0 +1,4 @@
+from secrets import FANTASY_NERD_API_KEY as api_key
+
+class PlayerPopulator(object):
+    pass
